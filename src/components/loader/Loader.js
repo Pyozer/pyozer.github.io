544f12b90f0631bdc81a0a10eb43e8a9e077b569
Jean-Charles Moussé <Jean-Charles.Mousse@intra.bodet-timesport.com>
2018-05-15 13:50:03 +0200

    Change loader and font

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import CenterElement from '../CenterElement';
+
 
 const Loader = () => (
     <CenterElement className="p-3">
-        <h1>Loading...</h1>
+        <h1>Loading <FontAwesomeIcon icon={["fab", "connectdevelop"]} size="xs" spin className="ml-2" /></h1>
     </CenterElement>
 );
 
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import CenterElement from '../components/CenterElement';
 
 const PageNotFound = ({ location }) => (
     <CenterElement className="m-5">
-        <h1 className="display-2 font-weight-bold">404</h1>
-        <h3 className="text-white">
-            No match for <code>{location.pathname}</code>
-        </h3> 
+        <h1 className="display-1 font-weight-bold text-shadow-effect mb-5">404</h1>
+        <h2 className="display-4 font-weight-normal">Looks like you're lost</h2>
+        <h3 className="font-weight-light mb-5">The page you are looking for not available !</h3>
+
+        <Link to="/" className="btn btn-outline-light btn-lg py-3 px-5 border-transparent">
+            <strong>GO TO HOME</strong> <FontAwesomeIcon icon={["fas", "arrow-right"]} className="ml-2" />
+        </Link>
     </CenterElement>
 );
 
