1f1dd5bef2aca8bd2e389ce03c66d46e4ef08688
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-05-22 03:49:29 +0200

    hide api key

---
+++ src/api/apiKeys.js
@@ -0,0 +1,8 @@
+export const firebaseKey = {
+    apiKey: "AIzaSyB4OinyE-e9baqVQEuqhnorU-Yzbdl3W1Y",
+    authDomain: "pyozer-github-io.firebaseapp.com",
+    databaseURL: "https://pyozer-github-io.firebaseio.com",
+    projectId: "pyozer-github-io",
+    storageBucket: "",
+    messagingSenderId: "720004809027"
+}
--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,15 +1,16 @@
 // Import the Firebase modules that you need in your app.
 import firebase from '@firebase/app';
 import '../../node_modules/firebase/database';
+import { firebaseKey } from './apiKeys';
 
 // Initalize and export Firebase.
 const config = {
-    apiKey: "AIzaSyB4OinyE-e9baqVQEuqhnorU-Yzbdl3W1Y",
-    authDomain: "pyozer-github-io.firebaseapp.com",
-    databaseURL: "https://pyozer-github-io.firebaseio.com",
-    projectId: "pyozer-github-io",
-    storageBucket: "",
-    messagingSenderId: "720004809027"
+    apiKey: firebaseKey.apiKey,
+    authDomain: firebaseKey.authDomain,
+    databaseURL: firebaseKey.databaseURL,
+    projectId: firebaseKey.projectId,
+    storageBucket: firebaseKey.storageBucket,
+    messagingSenderId: firebaseKey.messagingSenderId
 };
 
 export default firebase.initializeApp(config);
