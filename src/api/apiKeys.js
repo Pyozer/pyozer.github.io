fdf0d4de668adc952e215f999837ff1cd3a2969e
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-05-22 03:51:46 +0200

    update

--- src/api/apiKeys.js
+++
@@ -1,8 +0,0 @@
-export const firebaseKey = {
-    apiKey: "AIzaSyB4OinyE-e9baqVQEuqhnorU-Yzbdl3W1Y",
-    authDomain: "pyozer-github-io.firebaseapp.com",
-    databaseURL: "https://pyozer-github-io.firebaseio.com",
-    projectId: "pyozer-github-io",
-    storageBucket: "",
-    messagingSenderId: "720004809027"
-}
